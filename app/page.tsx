50a420fdbb6a6954d27e0de2afc836362b4e33cf
Rahul <rahulagdev@gmail.com>
2026-07-12 11:44:52 +0530

    Projects section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden sm:px-10 px-5">
@@ -11,6 +12,7 @@ export default function Home() {
         <FloatingNav navItems={[{name: "Home", link: "/", icon: <FaHome />}]} />
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
