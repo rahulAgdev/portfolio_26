c8d1056a8953cd9fd17dc3b73f1e9e65d469973e
Rahul <rahulagdev@gmail.com>
2026-07-15 20:19:56 +0530

    Deployment Attempt 1

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,11 @@ import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col sm:px-10 px-5 overflow-clip">
 
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems}/>
@@ -16,6 +18,8 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
